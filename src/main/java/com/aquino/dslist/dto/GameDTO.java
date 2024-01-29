c9c9b4a3d29e3b439aa2891087b0fe5d21ee7b92
Luiz Aquino <quinnpw@live.com>
2024-01-29 13:26:01 -0300

    "Get game by id"

---
+++ src/main/java/com/aquino/dslist/dto/GameDTO.java
@@ -0,0 +1,22 @@
+package com.aquino.dslist.dto;
+
+
+import com.aquino.dslist.entities.Game;
+
+public record GameDTO(
+        Long id,
+        String title,
+        Integer year,
+        String genre,
+        String platforms,
+        Double score,
+        String imgUrl,
+        String shortDescription,
+        String longDescription) {
+
+    public GameDTO (Game game) {
+        this(game.getId(), game.getTitle(), game.getYear(), game.getGenre(), game.getPlatforms(), game.getScore(), game.getImgUrl(), game.getShortDescription(), game.getLongDescription());
+    }
+
+
+}
--- src/main/java/com/aquino/dslist/services/GameService.java
+++ src/main/java/com/aquino/dslist/services/GameService.java
@@ -1,10 +1,12 @@
 package com.aquino.dslist.services;
 
+import com.aquino.dslist.dto.GameDTO;
 import com.aquino.dslist.dto.GameMinDTO;
 import com.aquino.dslist.entities.Game;
 import com.aquino.dslist.repository.GameRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -14,6 +16,14 @@ public class GameService {
     @Autowired
     private GameRepository gameRepository;
 
+    @Transactional(readOnly = true)
+    public GameDTO findById(Long id) {
+        Game result = gameRepository.findById(id).get();
+        GameDTO dto = new GameDTO(result);
+        return dto;
+    }
+
+    @Transactional(readOnly = true)
     public List<GameMinDTO> findAll() {
         var result = gameRepository.findAll();
         List<GameMinDTO> dto = result.stream().map(g -> new GameMinDTO(g)).toList();
