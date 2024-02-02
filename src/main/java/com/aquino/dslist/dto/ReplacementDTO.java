b22873ef0431cb6e5b7bcc5019e5fa14c1cb4970
Luiz Aquino <quinnpw@live.com>
2024-02-02 11:34:27 -0300

    Move games on a list

---
+++ src/main/java/com/aquino/dslist/dto/ReplacementDTO.java
@@ -0,0 +1,6 @@
+package com.aquino.dslist.dto;
+
+public record ReplacementDTO(
+        Integer sourceIndex,
+        Integer destinationIndex) {
+}
--- src/main/java/com/aquino/dslist/services/GameListService.java
+++ src/main/java/com/aquino/dslist/services/GameListService.java
@@ -1,7 +1,9 @@
 package com.aquino.dslist.services;
 import com.aquino.dslist.dto.GameListDTO;
 import com.aquino.dslist.entities.GameList;
+import com.aquino.dslist.projections.GameMinProjection;
 import com.aquino.dslist.repository.GameListRepository;
+import com.aquino.dslist.repository.GameRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -14,10 +16,27 @@ public class GameListService {
     @Autowired
     private GameListRepository gameListRepository;
 
+    @Autowired
+    private GameRepository gameRepository;
+
 
     @Transactional(readOnly = true)
     public List<GameListDTO> findAll() {
         List<GameList> result = gameListRepository.findAll();
         return result.stream().map(g -> new GameListDTO(g)).toList();
+    }
+
+    @Transactional
+    public void move(Long listId, int sourceIndex, int destinationIndex) {
+        List<GameMinProjection> list = gameRepository.searchByList(listId);
+        GameMinProjection obj = list.remove(sourceIndex);
+        list.add(destinationIndex, obj);
+
+        int min = sourceIndex < destinationIndex ? sourceIndex : destinationIndex;
+        int max = sourceIndex < destinationIndex ? destinationIndex : sourceIndex;
+
+        for (int i = min; i <= max; i++) {
+            gameListRepository.updateBelongingPosition(listId,list.get(i).getId(), i);
+        }
     }
 }
